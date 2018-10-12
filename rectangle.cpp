5dd4038f5d57da17839b5416a5d60eeef01c31b1
Martin Pouliot <martinp507@gmail.com>
2018-10-12 10:55:07 -0400

    Fix inline style

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,9 +1,8 @@
 #include "rectangle.h"
 
-Rectangle::Rectangle(double width, double height)
-: _width(width)
-, _height(height)
+Rectangle::Rectangle(double width, double height): _width(width), _height(height)
 {
+	
 }
 
 double Rectangle::getPerimeter()
